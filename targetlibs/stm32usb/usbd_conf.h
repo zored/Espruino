010bb812f698d0190da4a8890a30e911bf8e06bf
Harry Morgan <zotov.bmstu@gmail.com>
2017-02-02 21:15:38 +0300

    added .h file

--- targetlibs/stm32usb/usbd_conf.h
+++ targetlibs/stm32usb/usbd_conf.h
@@ -64,7 +64,7 @@
   */ 
 
 /*---------- -----------*/
-#define USBD_MAX_NUM_INTERFACES     1
+#define USBD_MAX_NUM_INTERFACES     2
 /*---------- -----------*/
 #define USBD_MAX_NUM_CONFIGURATION     1
 /*---------- -----------*/
